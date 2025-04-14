9a91a0c4f4db4b9adeffa18f2bc3011108049dfb
Connor-Karr <connorpkarr@gmail.com>
2025-04-14 16:57:50 -0400

    Made a get to get the macro data of an ingredient

--- api/backend/macros/macros_routes.py
+++ api/backend/macros/macros_routes.py
@@ -2,3 +2,7 @@ from flask import Blueprint, request, jsonify, make_response, current_app
 from backend.db_connection import db
 
 macros = Blueprint('macros', __name__)
+
+@macros.route('/', methods=['GET'])
+def get_macronutrients():
+    """Get macronutrient data"""
