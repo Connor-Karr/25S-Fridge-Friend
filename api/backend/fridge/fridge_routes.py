cb363e1496f215528740ba19067c68a2c345e7db
Connor-Karr <connorpkarr@gmail.com>
2025-04-14 16:18:20 -0400

    Making the sql query for the get current fridge

--- api/backend/fridge/fridge_routes.py
+++ api/backend/fridge/fridge_routes.py
@@ -7,6 +7,21 @@ fridge = Blueprint('fridge', __name__)
 def get_fridge_inventory():
     """Get current fridge inventory"""
 
+    cursor = db.get_db().cursor()
+    query = '''
+        SELECT fi.fridge_id, i.name, fi.quantity, i.expiration_date, fi.is_expired
+        FROM Fridge_Inventory fi
+        JOIN Client c ON c.fridge_id = fi.fridge_id
+        JOIN Ingredient i ON fi.ingredient_id = i.ingredient_id
+        WHERE c.client_id = %s
+    '''
+    cursor.execute(query, (client_id,))
+    inventory = cursor.fetchall()
+    
+    response = make_response(jsonify(inventory))
+    response.status_code = 200
+    return response
+
 @fridge.route('/<int:ingredient_id>', methods=['GET'])
 def get_fridge_ingredient(ingredient_id):
     """Get details for a specific ingredient"""
