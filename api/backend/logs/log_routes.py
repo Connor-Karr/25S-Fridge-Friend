e549b378b53f8917c8bc320fd01fe4fa32380f90
Connor-Karr <connorpkarr@gmail.com>
2025-04-14 17:12:42 -0400

    Made the sql query based on the client id

--- api/backend/logs/log_routes.py
+++ api/backend/logs/log_routes.py
@@ -10,3 +10,17 @@ def get_scan_history():
     client_id = request.args.get('client_id')
     
     cursor = db.get_db().cursor()
+    query = '''
+            SELECT fsl.*, i.name as ingredient_name
+            FROM Food_Scan_Log fsl
+            JOIN Ingredient i ON fsl.ingredient_id = i.ingredient_id
+            JOIN Client c ON c.log_id = fsl.log_id
+            WHERE c.client_id = %s
+            ORDER BY fsl.timestamp DESC
+        '''
+    cursor.execute(query, (client_id,))
+    scans = cursor.fetchall()
+    
+    response = make_response(jsonify(scans))
+    response.status_code = 200
+    return response
