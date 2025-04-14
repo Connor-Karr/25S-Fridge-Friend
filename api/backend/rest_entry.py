8d7bd528858f350939519c137135a2b13d46a3c9
ConCars <87278575+ConCars@users.noreply.github.com>
2025-04-14 14:52:54 -0400

    Removed original routes and add a new user route file. Edited rest_entry

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -40,8 +40,11 @@ def create_app():
     # and give a url prefix to each
     app.logger.info('current_app(): registering blueprints with Flask app object.')   
     app.register_blueprint(simple_routes)
-    app.register_blueprint(customers,   url_prefix='/c')
-    app.register_blueprint(products,    url_prefix='/p')
+    app.register_blueprint(users, url_prefix='/users')
+    app.register_blueprint(ingredients, url_prefix='/ingredients')
+    app.register_blueprint(recipes, url_prefix='/recipes')
+    app.register_blueprint(fridge, url_prefix='/fridge')
+    app.register_blueprint(shopping, url_prefix='/shopping')
 
     # Don't forget to return the app object
     return app
