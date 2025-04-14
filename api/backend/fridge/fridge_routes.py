11d5a5fbb08f6a062177ed0b2d8bcda1ccd8dc2d
Connor-Karr <connorpkarr@gmail.com>
2025-04-14 15:46:01 -0400

    Start of the frige inventory route

--- api/backend/fridge/fridge_routes.py
+++ api/backend/fridge/fridge_routes.py
@@ -2,3 +2,6 @@ from flask import Blueprint, request, jsonify, make_response, current_app
 from backend.db_connection import db
 
 fridge = Blueprint('fridge', __name__)
+
+@fridge.route('/inventory/<client_id>', methods=['GET'])
+def get_fridge_inventory(client_id):
