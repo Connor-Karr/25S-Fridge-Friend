418f52d566d0e33fd7021d99627e01899f496a94
“sneha-kondru” <sneha.kondru@gmail.com>
2025-04-14 16:44:39 -0400

    start get_ingredient route and cursor setup

--- api/backend/ingredients/ingredient_routes.py
+++ api/backend/ingredients/ingredient_routes.py
@@ -2,3 +2,9 @@ from flask import Blueprint, request, jsonify, make_response, current_app
 from backend.db_connection import db
 
 ingredients = Blueprint('ingredients', __name__)
+
+@ingredients.route('/<int:ingredient_id>', methods=['GET'])
+def get_ingredient(ingredient_id):
+    """Get ingredient details"""
+    cursor = db.get_db().cursor()
+
