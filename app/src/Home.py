de8392d6c7d105deab5563d004ffb84075e5caa1
jessicajain111 <jessicajain111@gmail.com>
2025-04-15 16:06:08 -0400

    start home.py by adding imports

---
+++ app/src/Home.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+import time
+from datetime import datetime, timedelta
+from PIL import Image
+import base64
+from modules.nav import SideBarLinks
+import plotly.express as px
+
+# Set page configuration
+st.set_page_config(
+    page_title="FridgeFriend",
+    page_icon="🍏",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
