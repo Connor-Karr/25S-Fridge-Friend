877b6618364f2468d604ecb36418bf429db14508
Connor-Karr <connorpkarr@gmail.com>
2025-04-15 12:07:51 -0400

    Added a get route for leftovers that gets all of the leftovers

--- api/backend/leftovers/leftover_routes.py
+++ api/backend/leftovers/leftover_routes.py
@@ -3,3 +3,30 @@ from backend.db_connection import db
 from datetime import datetime, timedelta
 
 leftovers = Blueprint('leftovers', __name__)
+
+@leftovers.route('/', methods=['GET'])
+def get_leftovers():
+    """Get all leftovers"""
+    recipe_id = request.args.get('recipe_id')
+    
+    cursor = db.get_db().cursor()
+    
+    if recipe_id:
+        cursor.execute('''
+            SELECT l.*, r.name as recipe_name
+            FROM Leftover l
+            JOIN Recipe r ON l.recipe_id = r.recipe_id
+            WHERE l.recipe_id = %s
+        ''', (recipe_id,))
+    else:
+        cursor.execute('''
+            SELECT l.*, r.name as recipe_name
+            FROM Leftover l
+            JOIN Recipe r ON l.recipe_id = r.recipe_id
+        ''')
+    
+    leftovers = cursor.fetchall()
+    
+    response = make_response(jsonify(leftovers))
+    response.status_code = 200
+    return response
