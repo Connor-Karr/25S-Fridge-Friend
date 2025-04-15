71e1af870ca221cf6760cb827d65b60d9a251656
jessicajain111 <jessicajain111@gmail.com>
2025-04-15 16:07:31 -0400

    session state initialization

--- app/src/Home.py
+++ app/src/Home.py
@@ -9,10 +9,21 @@ import base64
 from modules.nav import SideBarLinks
 import plotly.express as px
 
-# Set page configuration
+# page configuration
 st.set_page_config(
     page_title="FridgeFriend",
     page_icon="🍏",
     layout="wide",
     initial_sidebar_state="expanded"
 )
+
+
+if 'authenticated' not in st.session_state:
+    st.session_state.authenticated = False
+if 'role' not in st.session_state:
+    st.session_state.role = None
+if 'first_name' not in st.session_state:
+    st.session_state.first_name = None
+
+# sidebar navigation
+SideBarLinks(st.session_state.role)
