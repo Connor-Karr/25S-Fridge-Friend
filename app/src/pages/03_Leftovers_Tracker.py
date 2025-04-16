8bb2a90ddee3b5d2f13c6a5159b48a8efb76da52
aarushiattray <aarushiattray@gmail.com>
2025-04-15 21:55:08 -0400

    Setting up navigation

--- app/src/pages/03_Leftovers_Tracker.py
+++ app/src/pages/03_Leftovers_Tracker.py
@@ -13,3 +13,4 @@ API_BASE_URL = "http://web-api:4000"
 if not st.session_state.get('authenticated', False) or st.session_state.role != "busy_student":
     st.warning("Please log in as Ben to access this page")
     st.stop()
+
