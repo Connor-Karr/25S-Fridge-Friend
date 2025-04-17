6d83e8d411a6954cffb43afc83fb07d487afde51
Connor-Karr <connorpkarr@gmail.com>
2025-04-16 22:37:54 -0400

    Fixing imports

--- app/src/pages/13_User_Management.py
+++ app/src/pages/13_User_Management.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import requests
 from modules.nav import SideBarLinks
 
 if not st.session_state.get('authenticated', False) or st.session_state.role != "admin":
